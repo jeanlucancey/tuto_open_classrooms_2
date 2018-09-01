c4ada331d4d41ba17f70eb34f601344a7c68f263
Jean-Luc Ancey <triancey@gmail.com>
2018-09-01 09:37:22 +0200

    2018/09/01 (Sa) 09:37 Mini_URL en très bonne voie

--- mini_url/urls.py
+++ mini_url/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.accueil, name='accueil'),
-    path('accuse_reception', views.accuse_reception, name='accuse_reception'),
+    path('accueil', views.accueil, name='accueil'),
 ]
--- mini_url/views.py
+++ mini_url/views.py
@@ -11,30 +11,9 @@ def accueil(request):
     if form.is_valid():
         url_longue      = form.cleaned_data['url_longue']
         pseudo_createur = form.cleaned_data['pseudo_createur']
-        code_raccourci  = "http://jla." + generer(5)
-        date            = timezone.now
-        # nombre_acces    = form.cleaned_data['nombre_acces']
-        envoi = True
-        resultat = render(request, 'mini_url/accuse_reception.html', locals())
-    else:
-        resultat = render(request, 'mini_url/accueil.html', locals())
-    return resultat
-
-def generer(nb_caracteres):
-    caracteres = string.ascii_letters + string.digits
-    aleatoire = [random.choice(caracteres) for _ in range(nb_caracteres)]
-
-    return ''.join(aleatoire)
-
-def accuse_reception(request):
-    form = MiniURLForm(request.POST or None)
-    if form.is_valid():
-        url_longue      = form.cleaned_data['url_longue']
-        pseudo_createur = form.cleaned_data['pseudo_createur']
-        code_raccourci  = "http://jla." + generer(5)
+        code_raccourci  = "http://je_suis_accueil." + generer(5)
         date            = timezone.now()
         nombre_acces    = 0
-        # envoi = True
 
         monUrl = MiniURL()
         monUrl.url_longue = url_longue
@@ -43,4 +22,14 @@ def accuse_reception(request):
         monUrl.date = date
         monUrl.nombre_acces = nombre_acces
         monUrl.save()
-    return render(request, 'mini_url/accuse_reception.html', locals())
+
+        envoi = True
+    resultat = render(request, 'mini_url/accueil.html', locals())
+
+    return resultat
+
+def generer(nb_caracteres):
+    caracteres = string.ascii_letters + string.digits
+    aleatoire = [random.choice(caracteres) for _ in range(nb_caracteres)]
+
+    return ''.join(aleatoire)
